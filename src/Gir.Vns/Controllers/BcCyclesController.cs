eb7b9efa0f5e8d255be09b141cc428c9538ad978
Egor Kononenko <egorychmaster@yandex.ru>
2025-10-01 15:26:44 +0300

    2. Получение состояния шага (статус), возможные действия (завершение, согласование), загруженные шаблоны get core/BcCycle - BcCyclesController PUT /api/v1/gir/bcs/versions/cycles/batch Полное множественное редактирование цикла(шага) по ID.

---
+++ src/Gir.Vns/Controllers/BcCyclesController.cs
@@ -0,0 +1,24 @@
+﻿using Gir.Vns.Dtos.BcCycles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gir.Vns.Controllers;
+
+/// <summary>
+/// Цикл (шаг) данных версии БК.
+/// </summary>
+[ApiController]
+[Route("api/v1/gir/bcs/versions/cycles")]
+public class BcCyclesController : ControllerBase
+{
+    /// <summary>
+    /// Полное множественное редактирование цикла(шага) по ID.
+    /// </summary>
+    [HttpPut("batch")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public ActionResult UpdateComment([FromBody] IEnumerable<BcCycleUpdateCommand> dto)
+    {
+        return NoContent();
+    }
+}
---
+++ src/Gir.Vns/Dtos/BcCycles/BcCycleUpdateCommand.cs
@@ -0,0 +1,35 @@
+﻿using Gir.Vns.Dtos.BcCycles.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gir.Vns.Dtos.BcCycles;
+
+public class BcCycleUpdateCommand
+{
+    /// <summary>
+    /// ID ресурса, который требуется изменить.
+    /// </summary>
+    [Required]
+    public Guid Id { get; init; } = default!;
+
+    /// <summary>
+    /// DTO, содержащее данные для изменения ресурса.
+    /// </summary>
+    [Required]
+    public BcCycleUpdateDto Value { get; init; } = default!;
+}
+
+/// <summary>
+/// DTO изменения сущности "Цикл БК (данных версии БК)".
+/// </summary>
+public class BcCycleUpdateDto
+{
+    /// <summary>
+    /// Новый статус цикла БК. <br/>
+    /// См. `BcCycleStatusType` в `Schemas`.<br/>
+    /// Возможные значения:
+    /// 1 - Waiting - В ожидании.
+    /// 2 - InWork - В работе.
+    /// 3 - Finished - Завершен.
+    /// </summary>
+    public BcCycleStatusType StatusType { get; set; }
+}
