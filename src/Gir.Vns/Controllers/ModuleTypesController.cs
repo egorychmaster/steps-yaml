ffe4146872ec4ad0435e867b9341f1b84fe5eb30
Egor Kononenko <egorychmaster@yandex.ru>
2025-10-01 08:58:50 +0300

    14. Справочник модулей mdm/moduleType   GET /api/v1/gir/module-types Получение списка типов модулей с фильтрацией и сортировкой.

---
+++ src/Gir.Vns/Controllers/ModuleTypesController.cs
@@ -0,0 +1,25 @@
+﻿using Gir.Vns.Dtos;
+using Gir.Vns.Dtos.ModuleTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gir.Vns.Controllers;
+
+/// <summary>
+/// Типы модулей.
+/// </summary>
+[ApiController]
+[Route("api/v1/gir/module-types")]
+public class ModuleTypesController : ControllerBase
+{
+    /// <summary>
+    /// Получение списка типов модулей с фильтрацией и сортировкой.
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public ActionResult<CollectionResult<ModuleTypeDto>> GetModuleTypesAsync([FromQuery] GetModuleTypesQuery query)
+    {        
+        return Ok();
+    }
+}
---
+++ src/Gir.Vns/Dtos/ModuleTypes/GetModuleTypesQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace Gir.Vns.Dtos.ModuleTypes;
+
+public class GetModuleTypesQuery
+{
+    /// <summary>
+    /// Идентификаторы.
+    /// </summary>
+    public Guid[]? Ids { get; init; }
+}
