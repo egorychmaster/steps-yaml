9e7506594f20fe370c55fc33db63267aa0e51a5b
Egor Kononenko <egorychmaster@yandex.ru>
2025-10-02 17:46:07 +0300

    22. Справочник льгот mdm/benefit  BenefitsController GET /api/v1/gir/benefits Получение списка льгот с фильтрацией и сортировкой.

---
+++ src/Gir.Vns/Controllers/BenefitsController.cs
@@ -0,0 +1,25 @@
+﻿using Gir.Vns.Dtos.Benefits;
+using Microsoft.AspNetCore.Mvc;
+using Step.Lib.Common.Dtos;
+
+namespace Gir.Vns.Controllers;
+
+/// <summary>
+/// Льготы.
+/// </summary>
+[ApiController]
+[Route("api/v1/gir/benefits")]
+public class BenefitsController : ControllerBase
+{
+    /// <summary>
+    /// Получение списка льгот с фильтрацией и сортировкой.
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public ActionResult<CollectionResult<BenefitDto>> GetWellTypesAsync([FromQuery] GetBenefitsQuery query)
+    {
+        return Ok();
+    }
+}
