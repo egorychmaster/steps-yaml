f0b317a9c13ab5b8ba1181e6cb3ff368be7ba1d9
Egor Kononenko <egorychmaster@yandex.ru>
2025-10-08 17:25:57 +0300

    10 BcVersionSliceClustersController POST /api/v1/vns/{processType}/{bcVersionSliceId}/gir/clusters Создание куста относящегося к БК.

--- src/Gir.Vns/Controllers/BcVersionSliceClustersController.cs
+++ src/Gir.Vns/Controllers/BcVersionSliceClustersController.cs
@@ -9,7 +9,7 @@ namespace Gir.Vns.Controllers;
 /// </summary>
 [ApiController]
 [Route("api/v1/vns/{processType:alpha}/{bcVersionSliceId:guid}/gir/clusters")]
-public class BcVersionDataClustersController : ControllerBase
+public class BcVersionSliceClustersController : ControllerBase
 {
     /// <summary>
     /// Получение списка кустов в шаге.
@@ -21,5 +21,16 @@ public class BcVersionDataClustersController : ControllerBase
     public ActionResult<CollectionResult<BcVersionDataClusterDto>> GetLoadersAsync()
     {
         return Ok();
+    }
+
+    /// <summary>
+    /// Создание куста, относящегося к БК.
+    /// </summary>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public ActionResult<BcVersionDataClusterDto> CreateCluster([FromBody] BcClusterCreateDto dto)
+    {
+        return StatusCode(StatusCodes.Status201Created, new { Id = Guid.NewGuid() });
     }
 }
---
+++ src/Gir.Vns/Dtos/BcVersionDataClusters/BcClusterCreateDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Gir.Vns.Dtos.BcVersionDataClusters;
+
+public class BcClusterCreateDto
+{
+    /// <summary>
+    /// ID куста (из MDM сервиса).
+    /// </summary>
+    public Guid ClusterId { get; set; }
+
+    /// <summary>
+    /// ID Состава актива (из MDM сервиса), ассоциированного с данным кустом БК.
+    /// </summary>
+    public Guid AssetContentId { get; set; }
+}
