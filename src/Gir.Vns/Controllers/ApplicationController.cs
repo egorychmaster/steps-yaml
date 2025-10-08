6a998093d2c04d052073c0733be4622222061fc2
Egor Kononenko <egorychmaster@yandex.ru>
2025-10-08 16:06:24 +0300

    7 GET /api/v1/vns/{processType}/{bcVersionDataId}/gir/clusters

--- src/Gir.Vns/Controllers/ApplicationController.cs
+++ src/Gir.Vns/Controllers/ApplicationController.cs
@@ -3,22 +3,22 @@ using Step.Lib.Common.Dtos.ApplicationConfig;
 
 namespace Gir.Vns.Controllers;
 
-/// <summary>
-/// Приложение.
-/// </summary>
-[ApiController]
-[Route("api/v1/gir/application")]
-public class ApplicationController : ControllerBase
-{
-    /// <summary>
-    /// Получение конфигурации шага (доступные загрузчики, название, группа, тип).
-    /// </summary>
-    /// <returns></returns>
-    [HttpGet("config")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-    public ActionResult<ApplicationConfig> GetConfigs()
-    {
-        return Ok();
-    }
-}
+///// <summary>
+///// Приложение.
+///// </summary>
+//[ApiController]
+//[Route("api/v1/gir/application")]
+//public class ApplicationController : ControllerBase
+//{
+//    /// <summary>
+//    /// Получение конфигурации шага (доступные загрузчики, название, группа, тип).
+//    /// </summary>
+//    /// <returns></returns>
+//    [HttpGet("config")]
+//    [ProducesResponseType(StatusCodes.Status200OK)]
+//    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+//    public ActionResult<ApplicationConfig> GetConfigs()
+//    {
+//        return Ok();
+//    }
+//}
