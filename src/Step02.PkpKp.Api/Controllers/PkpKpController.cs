a1da8b994ab4c524099f8152ce60ae6798a342f6
Egor Kononenko <egorychmaster@yandex.ru>
2025-10-13 11:41:25 +0300

    PkpKp PkpKpController готов.

---
+++ src/Step02.PkpKp.Api/Controllers/PkpKpController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Step.Lib.Common.Dtos.BcCycles;
+
+namespace Step02.PkpKp.Api.Controllers;
+
+/// <summary>
+/// Шаг(цикл) данных версии БК.
+/// </summary>
+[ApiController]
+[Route("api/v1/vns/{processType:alpha}/{bcVersionSliceId:guid}/pkp-kp")]
+public class PkpKpController : ControllerBase
+{
+    /// <summary>
+    /// Вернуть данные цикла(шага) по ID, в том числе и его статус.
+    /// </summary>
+    /// <param name="id">Идентификатор шага.</param>
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public ActionResult<BcCycleDto> GetBcCycleById(Guid id)
+    {
+        return Ok();
+    }
+
+    /// <summary>
+    /// Редактирование шага(цикла) по ID.
+    /// </summary>
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public ActionResult UpdateBcCycle([FromBody] IEnumerable<BcCycleUpdateCommand> dto)
+    {
+        return NoContent();
+    }
+}
